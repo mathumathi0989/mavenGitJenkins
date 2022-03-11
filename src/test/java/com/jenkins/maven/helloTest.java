093f13fad4605d9307691fd95c02fd896bb046fb
Mathumathi Balakrishnan <mathumathi.b@gmail.com>
2022-03-11 10:31:28 -0500

    first mvn test

---
+++ src/test/java/com/jenkins/maven/helloTest.java
@@ -0,0 +1,13 @@
+package com.jenkins.maven;
+
+import org.junit.Test;
+
+public class helloTest {
+@Test
+	public void hello() {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello World");
+	}
+
+}
